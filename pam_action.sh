3779766e1f7ec86d8ffec31fd60ec4085825f9e8
Jakob Blomer <jblomer@cern.ch>
2014-03-17 15:07:41 +0100

    support secret contexts

--- pam_action.sh
+++ pam_action.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
 PIN=$1
-/usr/sbin/cernvm-online.sh pair "$PIN"
+SECRET=
+if echo "$PIN" | grep -q '[13579]$'; then
+  read -s -p "Context secret: " SECRET
+fi
+/usr/sbin/cernvm-online.sh pair "$PIN" "$SECRET"
